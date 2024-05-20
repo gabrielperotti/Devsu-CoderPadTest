8a01c936ddeec67e652ecd0155e5010eff9612a6
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-05-20 14:56:20 -0300

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import { ProductFormPageComponent } from './pages/product-form-page/product-form
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'product/list', pathMatch: 'full' },
+  { path: '', redirectTo: 'products/list', pathMatch: 'full' },
   { path: 'products/list', component: ProductListPageComponent },
   { path: 'products/form', component: ProductFormPageComponent },
-  { path: '**', pathMatch: 'full', component: NotFoundPageComponent }
+  { path: '**', component: NotFoundPageComponent }
 ];
