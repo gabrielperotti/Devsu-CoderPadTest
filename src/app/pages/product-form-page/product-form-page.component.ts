706aa3b6e9beb6bdd22962656e726d546b313e5b
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-05-20 18:58:42 -0300

    new product form

--- src/app/pages/product-form-page/product-form-page.component.ts
+++ src/app/pages/product-form-page/product-form-page.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { IProduct } from '../../shared/interfaces/product.interface';
+import { ProductsService } from '../../shared/services/products.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-form-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.css'
 })
-export class ProductFormPageComponent {
+export class ProductFormPageComponent implements OnInit {
+  public form!: UntypedFormGroup;
+  public product!: IProduct;
+  public submitted = false;
+  public formValueChanged = false;
 
+  private _ProductsService = inject(ProductsService);
+  private _FormBuilder = inject(UntypedFormBuilder);
+  private _Router = inject(Router);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.initializeComponent();
+  }
+
+  async initializeComponent() {
+    try {
+      this.buildForm();
+    } catch (e) {
+      console.log(e);
+      alert('error fetching data');
+    }
+  }
+
+  get f() { return this.form.controls }
+  buildForm() {
+    const now = new Date();
+    this.form = this._FormBuilder.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      logo: ['', Validators.required],
+      date_release: [now, Validators.required],
+      date_revision: [{ value: now, disabled: true }, Validators.required],
+    })
+
+    this.form.valueChanges.subscribe(() => {
+      this.formValueChanged = true;
+    });
+  }
+
+  return() {
+    this._Router.navigate(['/products', 'list']);
+  }
+
+  reset() {
+    this.form.reset();
+    this.formValueChanged = false;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.invalid) return;
+    const data: IProduct = this.form.value;
+    data.date_release = (data.date_release ? new Date(data.date_release) : new Date()).toISOString();
+    data.date_revision = (data.date_revision ? new Date(data.date_revision) : new Date()).toISOString();
+    if (this.product) {
+      const subscription = this._ProductsService.update(data).subscribe(() => {
+        this._Router.navigate(['/products', 'list']);
+        subscription.unsubscribe();
+      })
+    } else {
+      const subscription = this._ProductsService.create(data).subscribe(() => {
+        this._Router.navigate(['/']);
+        subscription.unsubscribe();
+      })
+    }
+  }
 }
--- src/app/pages/product-list-page/product-list-page.component.ts
+++ src/app/pages/product-list-page/product-list-page.component.ts
@@ -4,11 +4,12 @@ import { IProduct } from '../../shared/interfaces/product.interface';
 import { ProductsService } from '../../shared/services/products.service';
 import { firstValueFrom } from 'rxjs';
 import { ActionsDropdownComponent } from '../../shared/components/actions-dropdown/actions-dropdown.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-list-page',
   standalone: true,
-  imports: [CommonModule, ActionsDropdownComponent],
+  imports: [CommonModule, ActionsDropdownComponent, RouterLink],
   templateUrl: './product-list-page.component.html',
   styleUrl: './product-list-page.component.css'
 })
@@ -24,10 +25,7 @@ export class ProductListPageComponent implements OnInit {
 
   async initializeComponent() {
     try {
-      const date = new Date();
-      const tmp = date.getTime().toString();
       this.products = await firstValueFrom(this._ProductsService.getAll());
-      console.log(this.products);
     } catch (e) {
       console.log(e);
       alert('error fetching data');
