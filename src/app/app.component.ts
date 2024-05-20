f73427189e1a0a2840b459e6269a9e756dce827a
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-05-20 14:56:09 -0300

    layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductsService } from './shared/services/products.service';
-import { IProduct } from './shared/interfaces/product.interface';
-import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,25 +8,6 @@ import { firstValueFrom } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  private _ProductsService = inject(ProductsService);
-  public products: IProduct[] = [];
-
+export class AppComponent {
   constructor() { }
-
-  ngOnInit(): void {
-    this.initializeComponent();
-  }
-
-  async initializeComponent() {
-    try {
-      const date = new Date();
-      const tmp = date.getTime().toString();
-      this.products = await firstValueFrom(this._ProductsService.getAll());
-      console.log(this.products);
-    } catch (e) {
-      console.log(e);
-      alert('error fetching data');
-    }
-  }
 }
