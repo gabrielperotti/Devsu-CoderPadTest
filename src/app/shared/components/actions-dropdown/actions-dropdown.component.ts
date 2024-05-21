d1e04602186c5064e3babb97b39e284fd9e49d4a
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-05-21 17:10:56 -0300

    change detect strategy

--- src/app/shared/components/actions-dropdown/actions-dropdown.component.ts
+++ src/app/shared/components/actions-dropdown/actions-dropdown.component.ts
@@ -1,29 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, HostListener, OnInit, Output, inject } from '@angular/core';
 
 @Component({
   selector: 'app-actions-dropdown',
   standalone: true,
   imports: [CommonModule,],
   templateUrl: './actions-dropdown.component.html',
-  styleUrl: './actions-dropdown.component.css'
+  styleUrl: './actions-dropdown.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ActionsDropdownComponent {
+export class ActionsDropdownComponent implements OnInit {
   @Output('edit') edit = new EventEmitter();
   @Output('delete') delete = new EventEmitter();
   isOpen = false;
+  private _ChangeDetectorRef = inject(ChangeDetectorRef);
 
   constructor() { }
 
+  ngOnInit(): void {
+    
+  }
+
   toggleDropdown(event: Event) {
+    console.log('toggleDropdown');
     /* to avoid clickOutside trigger */
-    setTimeout(() => this.isOpen = !this.isOpen, 0);
+    setTimeout(() => {
+      this.isOpen = !this.isOpen;
+      this._ChangeDetectorRef.detectChanges();
+    }, 0);
   }
 
   @HostListener('document:click', ['event'])
   clickOutside(event: Event) {
+    console.log('clickOutside', this.isOpen)
     /* to avoid closing before opening */
     if (this.isOpen) this.isOpen = false;
+    this._ChangeDetectorRef.detectChanges();
   }
 
   editItem() {
