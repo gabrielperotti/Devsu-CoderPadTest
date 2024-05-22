37e0708cdb644a6077e8ad9da61f7f6f2999f517
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-05-22 10:31:20 -0300

    not found page test

--- src/app/pages/not-found-page/not-found-page.component.spec.ts
+++ src/app/pages/not-found-page/not-found-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NotFoundPageComponent } from './not-found-page.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('NotFoundPageComponent', () => {
   let component: NotFoundPageComponent;
@@ -8,10 +9,9 @@ describe('NotFoundPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NotFoundPageComponent]
-    })
-    .compileComponents();
-    
+      imports: [RouterTestingModule, NotFoundPageComponent]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NotFoundPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,16 @@ describe('NotFoundPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the correct title', () => {
+    const titleElement: HTMLElement = fixture.debugElement.query(By.css('h1')).nativeElement;
+    expect(titleElement.textContent).toContain('404 - Página no encontrada');
+  });
+
+  it('should have a button to return to the home page', () => {
+    const buttonElement = fixture.debugElement.query(By.css('button.return'));
+    expect(buttonElement).toBeTruthy();
+    expect(buttonElement.attributes['ng-reflect-router-link']).toBe('/');
   });
 });
