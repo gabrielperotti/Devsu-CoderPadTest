e6fbcf74d006c464c65b1d0b942ee77cfb1880e9
Gabriel Perotti <gabrielperotti@hotmail.com>
2024-05-22 11:42:47 -0300

    actions dropdown tests

--- src/app/shared/components/actions-dropdown/actions-dropdown.component.spec.ts
+++ src/app/shared/components/actions-dropdown/actions-dropdown.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ActionsDropdownComponent } from './actions-dropdown.component';
+import { CommonModule } from '@angular/common';
+import { By } from '@angular/platform-browser';
 
 describe('ActionsDropdownComponent', () => {
   let component: ActionsDropdownComponent;
@@ -8,10 +9,12 @@ describe('ActionsDropdownComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ActionsDropdownComponent]
+      imports: [CommonModule, ActionsDropdownComponent]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(ActionsDropdownComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +22,71 @@ describe('ActionsDropdownComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle dropdown open state when button is clicked', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', new Event('click'));
+    fixture.detectChanges();
+
+    expect(component.isOpen).toBe(true);
+
+    button.triggerEventHandler('click', new Event('click'));
+    fixture.detectChanges();
+
+    expect(component.isOpen).toBe(false);
+  });
+
+  it('should close dropdown when clicking outside', () => {
+    component.isOpen = true;
+    fixture.detectChanges();
+
+    document.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+
+    expect(component.isOpen).toBe(false);
+  });
+
+  it('should emit edit event when editItem is called', () => {
+    jest.spyOn(component.edit, 'emit');
+
+    component.editItem();
+
+    expect(component.edit.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('should emit delete event when deleteItem is called', () => {
+    jest.spyOn(component.delete, 'emit');
+
+    component.deleteItem();
+
+    expect(component.delete.emit).toHaveBeenCalledWith(true);
+  });
+
+  it('should not close the dropdown when clicking the button', () => {
+    component.isOpen = true;
+    fixture.detectChanges();
+
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', new Event('click'));
+    fixture.detectChanges();
+
+    expect(component.isOpen).toBe(true);
+  });
+
+  it('should render dropdown menu when isOpen is true', () => {
+    component.isOpen = true;
+    fixture.detectChanges();
+
+    const dropdownMenu = fixture.debugElement.query(By.css('.dropdown-menu'));
+    expect(dropdownMenu).toBeTruthy();
+  });
+
+  it('should not render dropdown menu when isOpen is false', () => {
+    component.isOpen = false;
+    fixture.detectChanges();
+
+    const dropdownMenu = fixture.debugElement.query(By.css('.dropdown-menu'));
+    expect(dropdownMenu).toBeFalsy();
   });
 });
